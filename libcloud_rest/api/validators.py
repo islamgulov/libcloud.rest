2aed3fcadf43a445580e5f85a4fae963bb7c0645
Ilgiz Islamgulov <ilgiz.islamgulov@gmail.com>
2012-06-12 03:00:39 +0600

    add basic validators

--- libcloud_rest/api/validators.py
+++ libcloud_rest/api/validators.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from libcloud_rest.errors import MissingHeaderError
 from libcloud_rest.api.parser import ARGS_TO_XHEADERS_DICT
+from libcloud_rest.exception import ValidationError
 
 __all__ = [
     'validate_header_arguments',
@@ -20,3 +21,61 @@ def validate_header_arguments(required_arguments, arguments):
             header_names = [ARGS_TO_XHEADERS_DICT[arg]
                             for arg in arg_altertives]
             raise MissingHeaderError(header=' or '.join(header_names))
+
+
+class BaseValidator(object):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.required = kwargs.pop('required', True)
+
+    def __call__(self, data):
+        """
+        Validate data
+        @return True if data correct
+        """
+        self.raw_data = data
+        self._validate()
+        return True
+
+    def _validate(self):
+        if not self.required and not self.raw_data:
+            return True
+        self._check_data()
+
+    def _check_data(self):
+        """
+        Validation function that does not check required flag.
+        """
+        raise NotImplemented('To use this class inherit from it '
+                             'and define _check_data method')
+
+
+class IntegerValidator(BaseValidator):
+    def _check_data(self):
+        try:
+            _ = int(self.raw_data)
+        except (ValueError, TypeError):
+            raise ValidationError('Data must be integer')
+
+
+class StringValidator(BaseValidator):
+    def _check_data(self):
+        if not isinstance(self.raw_data, basestring):
+            raise ValidationError('Data must be string')
+
+
+class DictValidator(BaseValidator):
+
+    def __init__(self, *args, **kwargs):
+        print args[0]
+        if not isinstance(args[0], dict):
+            raise TypeError('Argument must be dict')
+        self.items_validators = args[0]
+        super(DictValidator, self).__init__(args, kwargs)
+
+    def _check_data(self):
+        if not isinstance(self.raw_data, dict):
+            raise ValidationError('Data must be dict')
+        for key, validator in self.items_validators.iteritems():
+            validator(self.raw_data.get(key, None))
---
+++ tests/test_validators.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+import unittest2
+
+from libcloud_rest.api import validators
+from libcloud_rest.exception import ValidationError
+
+
+class TestParser(unittest2.TestCase):
+
+    def test_string(self):
+        string_validator = validators.StringValidator()
+        valid_string = 'abc'
+        invalid_type = 123
+        self.assertTrue(string_validator(valid_string))
+        self.assertRaises(ValidationError, string_validator, invalid_type)
+
+    def test_integer(self):
+        integer_validator = validators.IntegerValidator()
+        valid_integer = 123
+        valid_integer2 = 12.3
+        invalid_integer = '12.3'
+        self.assertTrue(integer_validator(valid_integer))
+        self.assertTrue(integer_validator(valid_integer2))
+        self.assertRaises(ValidationError, integer_validator, invalid_integer)
+
+    def test_dict(self):
+        dict_validator = validators.DictValidator(
+                {'arg1': validators.StringValidator()})
+        valid_dict = {'arg1': 'str'}
+        invalid_dict = {'arg1': 123}
+        invalid_dict2 = {'abc': 'str'}
+        self.assertTrue(dict_validator(valid_dict))
+        self.assertRaises(ValidationError, dict_validator, invalid_dict)
+        self.assertRaises(ValidationError, dict_validator, invalid_dict2)
