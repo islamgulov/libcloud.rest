a9ead90647ac29cd8978062063040bed182198d1
Ilgiz Islamgulov <ilgiz.islamgulov@gmail.com>
2012-06-15 05:44:26 +0600

    add check for check for the existence of  libcloud directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,15 @@
 # -*- coding:utf-8 -*-
 import sys
+from os.path import exists, join as pjoin
 
 LIBCLOUD_DIR = '/home/ilgiz/dev/libcloud'
 
 
 def _import_libcloud_test():
+    if not exists(pjoin(LIBCLOUD_DIR, 'test')):
+        print "Missing libcloud directory"
+        print "Maybe you forgot to change it in: tests/__init__.py"
+        sys.exit(1)
     sys.path.insert(0, LIBCLOUD_DIR)
     import test
     test
