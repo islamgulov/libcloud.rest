03d7850c5fc619bfa38fbf2312caab6d2a2b1510
Ilgiz Islamgulov <ilgiz.islamgulov@gmail.com>
2012-06-19 01:16:45 +0600

    add reloading with gevent's WSGI server

--- libcloud_rest/server.py
+++ libcloud_rest/server.py
@@ -8,6 +8,7 @@ sys.path.append(os.sep.join(
     os.path.dirname(os.path.abspath(__file__)).split(os.sep)[:-1]))
 
 from werkzeug.serving import run_simple
+import werkzeug
 from gevent.monkey import patch_all
 from gevent import wsgi
 from libcloud_rest.application import LibcloudRestApp
@@ -17,11 +18,16 @@ def cli_start_server():
     app = LibcloudRestApp()
 
     if __debug__:
-        run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
+        run_simple('127.0.0.1', 5000, app, use_debugger=True,
+                   use_reloader=True)
     else:
         patch_all()
-        ws = wsgi.WSGIServer(('127.0.0.1', 5000), app)
-        ws.serve_forever()
+
+        @werkzeug.serving.run_with_reloader
+        def run_server():
+            ws = wsgi.WSGIServer(('127.0.0.1', 5000), app)
+            ws.serve_forever()
+
 
 if __name__ == '__main__':
     cli_start_server()
