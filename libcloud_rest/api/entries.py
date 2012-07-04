017fae773518f8c18d74033c3da500590f697327
Ilgiz Islamgulov <ilgiz.islamgulov@gmail.com>
2012-07-04 06:26:21 +0600

    change entries

--- libcloud_rest/api/entries.py
+++ libcloud_rest/api/entries.py
@@ -1,81 +1,104 @@
 # -*- coding:utf-8 -*-
-from functools import partial
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
-from libcloud_rest.exception import NoSuchObjectError
 from libcloud_rest.api import validators as valid
+from libcloud_rest.exception import MalformedJSONError
 
 
-class BasicEntry(object):
-    render_attrs = []  # list of object attributes which added to json
-    #list of attributes and their types which required to create object
-    arguments = {}  # type: validator
+class Field(object):
+    """
+    Base class for all field types.
+    """
+    validator_cls = None
+
+    def __init__(self, description=None, name=None, required=True):
+        self.description = description
+        self.name = name
+        self.required = required
+        self.validator = self.validator_cls(required=required, name=name)
+
+    def validate(self, json_data):
+        data = json_data.get(self.name, None)
+        if self.required or data:
+            self.validator(data)
+
+    def contribute_to_class(self, cls, name):
+        self.model = cls
+        self.name = name
+        self.validator.name = name
+
+
+class StringField(Field):
+    validator_cls = valid.StringValidator
+
+
+class EntryBase(type):
+    """
+    Metaclass for all entries.
+    """
+
+    def __new__(cls, name, bases, attrs):
+        super_new = super(EntryBase, cls).__new__
+        parents = [b for b in bases if isinstance(b, EntryBase)]
+        if not parents:
+            # If this isn't a subclass of Model, don't do anything special.
+            return super_new(cls, name, bases, attrs)
+        # Create the class.
+        module = attrs.pop('__module__', None)
+        new_class = super_new(cls, name, bases, {'__module__': module})
+        new_class.add_to_class('_fields', [])
+
+        # Add all attributes to the class.
+        for obj_name, obj in attrs.items():
+            new_class.add_to_class(obj_name, obj)
+
+        return new_class
+
+    def add_to_class(cls, name, value):
+        if isinstance(value, Field):
+            value.contribute_to_class(cls, name)
+            cls._fields.append(value)
+        else:
+            setattr(cls, name, value)
+
+
+class Entry(object):
+    __metaclass__ = EntryBase
 
     @classmethod
-    def to_json(cls, obj):
-        return dict(
-            ((a_name, getattr(obj, a_name)) for a_name in cls.render_attrs)
-        )
-
-    @classmethod
-    def validate_input_json(cls, params):
-        args = {}
-        for name, validator in cls.arguments.iteritems():
-            data = params.get(name, None)
-            if validator.required or (data is not None):
-                validator(data)
-                args[name] = data
-        return args
-
-    @classmethod
-    def _get_object(cls, arguments, driver):
-        pass
-
-    @classmethod
-    def from_json(cls, params, driver=None):
-        args = cls.validate_input_json(params)
-        return cls._get_object(args, driver)
+    def validate(cls, data):
+        try:
+            json_data = json.loads(data)
+        except ValueError, e:
+            raise MalformedJSONError(detail=str(e))
+        for field in cls._fields:
+            field.validate(json_data)
 
 
-class NodeEntry(BasicEntry):
-    render_attrs = ['id', 'name', 'state', 'public_ips']
-    arguments = {'node_id': valid.StringValidator()}
-
-    @classmethod
-    def _get_object(cls, arguments, driver):
-        nodes_list = driver.list_nodes()
-        node_id = arguments['node_id']
-        for node in nodes_list:
-            if node_id == node.id:
-                return node
-        raise NoSuchObjectError(obj_type='Node')
+class NodeEntry(Entry):
+    node_id = StringField('ID of the size which should be used')
 
 
-class ZoneEntry(BasicEntry):
-    render_attrs = ['id', 'domain', 'type', 'ttl']
-    arguments = {'zone_id': valid.StringValidator()}
-
-    @classmethod
-    def _get_object(cls, driver, arguments):
-        zone_id = arguments['zone_id']
-        return driver.get_zone(zone_id)
-
-
-def create_basic_entry(typename, validator):
-    @classmethod
-    def _get_object(cls, arguments, driver=None):
-        val = arguments[typename]
-        return type(typename.capitalize(), (), {typename: val})
-
-    type_dict = {'render_attrs': [typename],
-                 'arguments': {typename: validator},
-                 '_get_object': _get_object}
-    return type(typename.capitalize() + 'Entry', (BasicEntry, ), type_dict)
-
-create_string_entry = partial(create_basic_entry,
-                              validator=valid.StringValidator())
-
-LIBCLOUD_TYPES_ENTRIES = {
-    'L{Node}': NodeEntry,
-    'L{Zone}': ZoneEntry,
-    'zone_id': create_string_entry('zone_id'),
+simple_types_fields = {
+    'C{str}': StringField,
 }
+
+complex_entries = {
+    'L{Node}': NodeEntry,
+}
+
+
+def create_simple_entry(name, typename, description):
+    entry_name = name.capitalize() + 'Entry'
+    entry_field = simple_types_fields[typename](description)
+    return type(entry_name, (Entry,), {name: entry_field})
+
+
+def get_entry(name, typename, description=''):
+    if typename in simple_types_fields:
+        return create_simple_entry(name, typename, description)
+    elif typename in complex_entries:
+        raise NotImplementedError
--- tests/test_entries.py
+++ tests/test_entries.py
@@ -1,59 +1,20 @@
 # -*- coding:utf-8 -*-
 import unittest2
-from libcloud.compute.base import Node, NodeState
-from libcloud.compute.drivers.cloudstack import CloudStackNodeDriver
 
-from libcloud_rest.api.entries import LIBCLOUD_TYPES_ENTRIES
-from libcloud_rest.exception import NoSuchObjectError, ValidationError
-from tests.utils import get_test_driver_instance
+from libcloud_rest.api.entries import get_entry
+from libcloud_rest.exception import MalformedJSONError, ValidationError
 
 
-class TestNodeEntry(unittest2.TestCase):
+class StringEntryTests(unittest2.TestCase):
     def setUp(self):
-        self. entry = LIBCLOUD_TYPES_ENTRIES['L{Node}']
-        self.driver = get_test_driver_instance(CloudStackNodeDriver,
-                                               secret='apikey', key='user',
-                                               host='api.dummy.com',
-                                               path='/test/path')
+        self.entry = get_entry('zone_id', 'C{str}',
+                               '"ID of the zone which is required')
 
-    def test_to_json(self):
-        node = Node('111', 'test', NodeState.RUNNING, ['123.123.123.123'],
-                    None, Node)
-        json_data = self.entry.to_json(node)
-        self.assertEqual(node.name, json_data['name'])
-        self.assertEqual(node.id, json_data['id'])
-        self.assertEqual(node.state, json_data['state'])
-        self.assertEqual(node.public_ips, json_data['public_ips'])
-
-    def test_from_json(self):
-        json_data = {'node_id': '2600'}
-        node = self.entry.from_json(json_data, self.driver,)
-        self.assertEqual(node.id, json_data['node_id'])
-        bad_json_data = {'node_id': '0062'}
-        self.assertRaises(NoSuchObjectError, self.entry.from_json,
-                          bad_json_data, self.driver)
-
-    def test_validator(self):
-        json_data = {'node_id': '2600', 'unknowd_arg': 123}
-        node = self.entry.from_json(json_data, self.driver)
-        bad_json_data = {'node': '2600'}
-        self.assertRaises(ValidationError, self.entry.from_json,
-                          bad_json_data, self.driver)
-        bad_json_data = {'node_id': 2600}
-        self.assertRaises(ValidationError, self.entry.from_json,
-                          bad_json_data, self.driver)
-
-
-class TestZoneIDEntry(unittest2.TestCase):
-    def setUp(self):
-        self.entry = LIBCLOUD_TYPES_ENTRIES['zone_id']
-
-    def test_to_json(self):
-        zone_id = type('ZoneID', (), {'zone_id': '123'})
-        json_data = self.entry.to_json(zone_id)
-        self.assertEqual(zone_id.zone_id, json_data['zone_id'])
-
-    def test_from_json(self):
-        json_data = {'zone_id': '2600'}
-        zone_id = self.entry.from_json(json_data)
-        self.assertEqual(zone_id.zone_id, '2600')
+    def test_validate(self):
+        valid_json = '{"zone_id": "123"}'
+        self.entry.validate(valid_json)
+        malformed_json = "{ '1'}"
+        self.assertRaises(MalformedJSONError, self.entry.validate,
+                          malformed_json)
+        invalid_json = '{"zone_id": 123}'
+        self.assertRaises(ValidationError, self.entry.validate, invalid_json)
